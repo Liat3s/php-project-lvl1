8a47dfb5ceca9a0f3997a3f61ab409656981f557
Alex <pioner9393@gmail.com>
2021-10-08 00:55:19 +0700

    step3 done

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Php\Project\Lvl1\cli;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function welcome()
+{
+    \cli\line('Welcome to the Brain Game!');
+    $name = \cli\prompt('May I have your name?');
+    \cli\line("Hello, %s!", $name);
+}
