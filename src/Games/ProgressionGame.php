b6cfb726277187f7d63965a1ba3f5af2faf74b8e
a.plotnikov <a.plotnikov@robo.finance>
2021-10-28 04:20:37 +0700

    fix require vendor dir

--- src/Games/ProgressionGame.php
+++ src/Games/ProgressionGame.php
@@ -3,7 +3,13 @@
 namespace Php\Project\Lvl1\Games\ProgressionGame;
 
 // phpcs:disable
-require 'vendor/autoload.php';
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
 // phpcs:enable
 
 use function Php\Project\Lvl1\Engine\showMessage;
