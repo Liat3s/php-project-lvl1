3a44c2ce48d0128d004582ab4187151fbe75164b
a.plotnikov <a.plotnikov@robo.finance>
2021-10-28 04:44:23 +0700

    fix engine vendor dir

--- src/Engine.php
+++ src/Engine.php
@@ -3,7 +3,16 @@
 namespace Php\Project\Lvl1\Engine;
 
 // phpcs:disable
-require 'vendor/autoload.php';
+// Путь который будет использован при глобальной установке пакета
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+// Путь для локальной работы с проектом
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
 // phpcs:enable
 
 use function cli\line;
